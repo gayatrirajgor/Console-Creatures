82652a1a1eb56d7e64617f1e4f612857ef333716
Gayatri R <gayatrirajgor@gmail.com>
2023-09-30 21:21:15 +0100

    Added ghost's eyes

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // This will explain to the console what to write - in this case a ghost 
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");    
+    }
+  }
+}
