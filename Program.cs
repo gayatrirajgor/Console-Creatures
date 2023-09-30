49d253da3eb28eab392022baf0e5ad1fac3521a6
Gayatri R <gayatrirajgor@gmail.com>
2023-09-30 21:26:39 +0100

    adjusted spacing

--- Program.cs
+++ Program.cs
@@ -7,11 +7,12 @@ namespace ConsoleCreatures
     static void Main()
     {
       // This will explain to the console what to write - in this case a ghost 
-      Console.WriteLine(" .-.");
-      Console.WriteLine("(o o)");    
-      Console.WriteLine("| O |"); 
-      Console.WriteLine("|   |");  
-      Console.WriteLine("'~~~'");  
+      Console.WriteLine("  .-. ");
+      Console.WriteLine("( o o )");    
+      Console.WriteLine("|  O  |"); 
+      Console.WriteLine("|     |");  
+      Console.WriteLine("' ~~~ '");  
+      Console.WriteLine("Spiro the Specter");  
     }
   }
 }
